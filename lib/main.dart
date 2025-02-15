0abec87c95fbe29a88343f4254d3fd9d2ed5c3dc
Saleh Akram Sifat <salehakramsifat@gmail.com>
2025-02-16 02:13:16 +0600

    Initial Commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,8 +14,15 @@ class MyApp extends StatelessWidget{
   }
 }
 
-class HomeActivity extends StatelessWidget{
+class HomeActivity extends StatefulWidget{
   const HomeActivity({super.key});
+
+  @override
+  State<HomeActivity> createState() => _HomeActivityState();
+}
+
+class _HomeActivityState extends State<HomeActivity> {
+  bool isChecked = false;
 
   @override
   Widget build(BuildContext context) {
@@ -29,14 +36,21 @@ class HomeActivity extends StatelessWidget{
         children: [Center(child: CircleAvatar(radius: 50,
         backgroundColor: Colors.purpleAccent,),),
         SizedBox(height: 10,),
-        Center(child: Text("Saleh Akram Sifat", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.deepPurpleAccent),),)
+        Center(child: Text("Saleh Akram Sifat", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30, color: Colors.deepPurpleAccent),),),
 
+          SizedBox(height: 20,),
+          Row(
+            children: [Icon(Icons.email),SizedBox(width: 5,), Text("Salehakramsifat@gmail.com", style: TextStyle(fontSize: 15),)],),
+          SizedBox(height: 20,),
+          TextField(decoration:  InputDecoration(labelText: "Enter your name",border: OutlineInputBorder()),),
 
-
-
-
-
-
+          SizedBox(height: 5,),
+          CheckboxListTile(title:Text("I Agree to this terms"),value: isChecked, onChanged: (bool? value){
+            setState((){
+              isChecked=value!;
+            }
+            );
+          })
 
         ],),),
 
